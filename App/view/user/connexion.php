6bd9c805ff6f043d47f795acb4f1a22723cdc908
AbdelTRS <abdeltrs@hotmail.com>
2021-01-07 20:16:35 +0100

    add style for login register

--- App/view/user/connexion.php
+++ App/view/user/connexion.php
@@ -1,34 +1,32 @@
 <!DOCTYPE html>
 <html lang="en">
-
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-
-<body>
-
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="../App/css/style.css">
+        <title>IndoloR: Connexion</title>
+    </head>
     <body>
-
-        <div class="center">
-            <h2>Connexion</h2>
-            <br><br>
+        <a href="?page=home">
+            <img src="../App/img/logo.svg" alt="Logo">
+        </a>
+        <div class="box">
+            <h1>Connexion</h1>
             <form method="POST" action="">
-                <input type="email" name="mailConnect" placeholder="Mail">
-                <input type="password" name="mdpConnect" placeholder="Mot de passe">
-                <input type="submit" name="formConnect" value="Se connecter !">
-            </form>
-            <a href="?page=inscription">Vous inscrire !</a>
-            <a href="?page=home">Home</a>
-            <?php
+                <label for="email">Adresse mail</label>
+                <input type="email" name="mailConnect">
 
-            if (isset($erreur)) {
-                echo $erreur;
-            }
+                <label for="password">Mot de passe</label>
+                <input type="password" name="mdpConnect">
+                <div class="btnPosition">
+                    <input type="submit" name="formConnect" value="Se connecter" class="btn">
+                </div>
+            </form>
+            <?php
+                if (isset($erreur)) {
+                    echo $erreur;
+                }
             ?>
         </div>
     </body>
-</body>
-
 </html>
